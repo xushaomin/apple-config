5833186836607d99358d3a5eebc51eafdc848633
xushaomin <xushaomin@foxmail.com>
2017-09-16 18:34:40 +0800

    update

--- apple-config-diamond/src/main/java/com/appleframework/config/ExtendedPropertyPlaceholderConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.appleframework.config;
-
-import com.appleframework.config.core.spring.BasePropertyPlaceholderConfigurer;
-
-public class ExtendedPropertyPlaceholderConfigurer extends BasePropertyPlaceholderConfigurer {
-
-}
